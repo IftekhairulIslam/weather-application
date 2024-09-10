74b01647c97b777978a45d6263a1a7c3373bd8ca
Iftekhairul Islam <iftekhairul.islam@gmail.com>
2024-09-10 17:34:32 +0600

    fix: application icon and backgroud path after building application

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,21 +1,21 @@
-import { useState } from "react";
-import DashboardBody from "./DashboardBody";
-import DashboardHeader from "./DashboardHeader";
+import { useState } from 'react';
+import DashboardBody from './DashboardBody';
+import DashboardHeader from './DashboardHeader';
 
 const Dashboard = () => {
-    const defaultFilters = {
-        cityName: localStorage.getItem('cityName') ?? '',
-    };
-    const [filter, setFilter] = useState(defaultFilters);
+  const defaultFilters = {
+    cityName: localStorage.getItem('cityName') ?? '',
+  };
+  const [filter, setFilter] = useState(defaultFilters);
 
-    return (
-        <div className="h-full w-full" style={{backgroundImage: 'url(/src/assets/images/background-image.jpg)'}}>
-            <div className="h-full w-full md:w-[600px] mx-auto flex flex-col gap-5 p-8">
-                <DashboardHeader filter={filter} onFilterChange={setFilter}/>
-                <DashboardBody filter={filter} />
-            </div>
-        </div>
-    );
+  return (
+    <div className='h-full w-full bg-[url(@/assets/images/background-image.jpg)]'>
+      <div className='h-full w-full md:w-[600px] mx-auto flex flex-col gap-5 p-8'>
+        <DashboardHeader filter={filter} onFilterChange={setFilter} />
+        <DashboardBody filter={filter} />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
