e04b3d5741e77b1d4901253eca4eb2168e21664e
Iftekhairul Islam <iftekhairul.islam@gmail.com>
2024-09-11 00:14:35 +0600

    fix: ready to use environment variables

--- src/configuration/config.js
+++ src/configuration/config.js
@@ -1,3 +1,9 @@
-// Configuration should come from .env file
+// Base URL for OpenWeatherMap API requests
 export const BASE_URL = 'https://api.openweathermap.org/data/2.5';
+
+// API Key for OpenWeatherMap
 export const OPEN_WEATHER_MAP_API_KEY = 'df655e6eddf5064f0fff064d088ac9f3';
+
+// import getEnv from '../utilities/getEnv';
+// export const BASE_URL = getEnv('VITE_BASE_URL');
+// export const OPEN_WEATHER_MAP_API_KEY = getEnv('VITE_OPEN_WEATHER_MAP_API_KEY');
